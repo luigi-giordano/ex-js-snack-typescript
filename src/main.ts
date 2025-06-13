5c52349ce6bab03e6399b97b5aa63105f3ec4081
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-13 16:21:54 +0200

    [snack 3][bonus][completed]

--- src/main.ts
+++ src/main.ts
@@ -84,3 +84,10 @@ type ProjectManager = Dipendente & {
   budgetGestito?: number,
   stakeHolderPrincipali: string[]
 }
+
+type Team = {
+  nome: string,
+  progettoAttuale: string | null,
+  budget: number,
+  membri: [ProjectManager, Developer, ...Developer[]]
+}
