5e1b0c97c2c6fcdea93f6fc2bc61019df05f6d6b
luigi-giordano <luigi.giordano1992@gmail.com>
2025-06-13 16:19:12 +0200

    [snack 3][completed]

--- src/main.ts
+++ src/main.ts
@@ -40,9 +40,28 @@ if (typeof dato === 'string') {
 
 // 🎯 BONUS
 // Il type alias Dipendente, ha anche i seguenti dati:
-
 // emailAziendale → Email assegnata al dipendente (non si può modificare)
 // contratto → Specifica il tipo di contratto del dipendente, con valori limitati a “indeterminato”, “determinato” o “freelance”.
+
+// 🏆 Snack 3
+// Estendiamo Dipendente per definire due ruoli specifici all'interno dell'azienda:
+
+// Developer
+// livelloEsperienza → Il livello di esperienza del developer (le scelte possibili sono solo “Junior”, “Mid” o “Senior”).
+// linguaggi → Un array contenente i linguaggi di programmazione utilizzati dal developer in azienda (opzionale, perché i neo assunti non hanno ancora dei linguaggi assegnati).
+// certificazioni → Un array di stringhe contenente certificazioni tecniche ottenute dal developer (può essere vuoto).
+// ProjectManager
+// teamSize → Il numero di persone nel team gestito dal Project Manager (può essere null se non ha ancora un team assegnato).
+// budgetGestito → Il totale del budget annuale gestito dal PM (opzionale).
+// stakeholderPrincipali → Un array di stringhe con i nomi dei principali stakeholder con cui il PM collabora (può essere vuoto).
+
+// 🎯 BONUS
+// Definiamo un nuovo type alias Team, che rappresenta un gruppo di lavoro all'interno dell'azienda:
+
+// nome → Nome del team (stringa).
+// progettoAttuale → Nome del progetto su cui lavora il team (può essere null se il team è in attesa di assegnazione).
+// budget → Importo numerico del budget assegnato al team (sempre presente).
+// membri → Una tuple in cui il primo elemento è sempre un Project Manager, seguito da uno o più Developers (almeno un developer obbligatorio).
 
 type Dipendente = {
   nome: string,
@@ -52,4 +71,16 @@ type Dipendente = {
   anniDiServizio: number[],
   readonly emailAziendale: string,
   contratto: 'indeterminato' | 'determinato' | 'freelance'
+}
+
+type Developer = Dipendente & {
+  livelloEsperienza: 'Junior' | 'Mid' | 'Senior',
+  linguaggi?: string[],
+  certificazioni: string[]
+}
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null,
+  budgetGestito?: number,
+  stakeHolderPrincipali: string[]
 }
